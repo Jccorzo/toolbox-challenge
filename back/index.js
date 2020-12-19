7fb6f70f537af46a8906fc0b27fdbacf49f4973e
Camilo Corzo <ccorzo26@gmail.com>
2020-12-19 11:54:11 -0500

    creando funcion de reverse recursiva

--- back/index.js
+++ back/index.js
@@ -8,3 +8,32 @@ app.get('/iecho', (req, res) => {
 app.listen(3000, () => {
     console.log('Server started')
 })
+
+const invertir = (word='') => {
+    let length = word.length;
+    let inverseWord = "";
+
+    while (x >= 0) {
+        inverseWord = inverseWord + word.charAt(x);
+        length--;
+    }
+
+    return inverseWord;
+}
+
+const reverse = (word='') => {
+    if(word.length > 0){
+        return reverse(word.slice(1)) + word.slice(0,1)
+    } else {
+        return ''
+    }
+}
+
+
+const validateInverse = (original, inverse) => {
+    if(original == inverse){
+        return true
+    } else {
+        return false
+    }
+}
